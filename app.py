aeea957f2a82c78e32f83002a96f5ba72be237d2
Casey Juergens <79878212+caseyjuergens@users.noreply.github.com>
2021-07-13 19:55:05 -0700

    reworked scrape_mars and mission to mars

--- app.py
+++ app.py
@@ -6,17 +6,13 @@ import scrape_mars
 app = Flask(__name__)
 
 # Use pymongo to set up mongo connection
-conn="mongodb://localhost:27017"
-client=PyMongo.MongoClient(conn)
-
-#create mars db
-db=client.mars_db
-mars= db.mars
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+client=PyMongo(app)
 
 @app.route("/")
 def index():
     #find one record of data from the database
-    mars_data = mars.find_one()
+    mars_data = client.db.mars.find_one()
     #return template and data
     return render_template("index.html", dict=mars_data)
 
@@ -26,7 +22,7 @@ def scrape():
     #run scrape function
     scrape_dict = scrape_mars.scrape()
     #update mongo database 
-    mars.update({}, scrape_dict, upsert=True)
+    client.db.mars.update({}, scrape_dict, upsert=True)
     #redirect back to homepage
     return redirect("/")
 
--- scrape_mars.py
+++ scrape_mars.py
@@ -60,142 +60,21 @@ def scrape():
     hemis= soup.find_all('h3', limit=4)
     print(hemis)
 
-    #find title cerberus, get pic url
-    for x in range(1):
-        #start path to hemi full pic url
-        cerberus= soup.find_all('div', class_='item')
-        #print(cerberus)
-        
-        #click link
-        browser.click_link_by_partial_text('Cerberus Hemisphere Enhanced')
-        
-        html=browser.html
-        soup= bs(html, 'html.parser')
-        
-        #get hemi name
-        c_title=soup.find('h2', class_="title").text
-        
-        #finding pic url
-        downloads= soup.find('div', class_='downloads')
-        li=downloads.find('li')
-        url=li.find('a')['href']
-        #print(url)
-        
-        #add full image url to original url
-        cerb_url= hemi_url+url
-        #print(cerb_url)
-        
-        #create dict of hemi name and url
-        hemi_dict={
-            "title": c_title,
-            "url": cerb_url
-        }
-        print(hemi_dict)
+    hemispheres_dict={'title':[hemis],'img_url':[]}
+    print(hemispheres_dict)
 
     #reset browser
     hemi_url='https://marshemispheres.com/'
     browser.visit(hemi_url)
     html= browser.html
-    soup= bs(html, 'html.parser') 
+    soup= bs(html, 'html.parser')
 
-    #find title schiaparelli, get pic url
-    for x in range(2):
-        #start path to hemi full pic url
-        schia= soup.find_all('div', class_='item')
-        #print(schia)
-        
-        #click link
-        browser.click_link_by_partial_text('Schiaparelli Hemisphere Enhanced')
-        
-        html=browser.html
-        soup= bs(html, 'html.parser')
-        
-        #get hemi name
-        s_title=soup.find('h2', class_="title").text
-        
-        #finding pic url
-        downloads= soup.find('div', class_='downloads')
-        li=downloads.find('li')
-        url=li.find('a')['href']
-        #print(url)
-        
-        #add full image url to original url
-        schia_url= hemi_url+url
-        #print(schia_url)
-        
-        #create dict of hemi name and url
-        hemi_dict[s_title]= schia_url
-        print(hemi_dict)
+    for i in range(4):
+        browser.find_by_css('a.itemLink h3')[i].click()
+        hemispheres_dict['img_url']= browser.find_by_text('Sample')['href']
+        browser.back()
+    print(hemispheres_dict)
 
-    #reset browser
-    hemi_url='https://marshemispheres.com/'
-    browser.visit(hemi_url)
-    html= browser.html
-    soup= bs(html, 'html.parser') 
-
-    #find title syrtis major, get pic url
-    for x in range(3):
-        #start path to hemi full pic url
-        syrtis= soup.find_all('div', class_='item')
-        #print(syrtis)
-        
-        #click link
-        browser.click_link_by_partial_text('Syrtis Major Hemisphere Enhanced')
-        
-        html=browser.html
-        soup= bs(html, 'html.parser')
-        
-        #get hemi name
-        syr_title=soup.find('h2', class_="title").text
-        
-        #finding pic url
-        downloads= soup.find('div', class_='downloads')
-        li=downloads.find('li')
-        url=li.find('a')['href']
-        #print(url)
-        
-        #add full image url to original url
-        syrtis_url= hemi_url+url
-        #print(syrtis_url)
-        
-        #create dict of hemi name and url
-        hemi_dict[syr_title]= syrtis_url
-        print(hemi_dict)
-        #reset browser
-
-    hemi_url='https://marshemispheres.com/'
-    browser.visit(hemi_url)
-    html= browser.html
-    soup= bs(html, 'html.parser') 
-
-    #find title valles marineris, get pic url
-    for x in range(4):
-        #start path to hemi full pic url
-        valles= soup.find_all('div', class_='item')
-        #print(valles)
-        
-        #click link
-        browser.click_link_by_partial_text('Valles Marineris Hemisphere Enhanced')
-        
-        html=browser.html
-        soup= bs(html, 'html.parser')
-        
-        #get hemi name
-        v_title=soup.find('h2', class_="title").text
-        
-        #finding pic url
-        downloads= soup.find('div', class_='downloads')
-        li=downloads.find('li')
-        url=li.find('a')['href']
-        #print(url)
-        
-        #add full image url to original url
-        valles_url= hemi_url+url
-        #print(valles_url)
-        
-        #create dict of hemi name and url
-        hemi_dict[v_title]= valles_url
-        print(hemi_dict)
 
     browser.quit()
 
@@ -203,7 +82,7 @@ def scrape():
         "news_title": news_title,
         "news_p": news_p,
         "featured_image_url": featured_image_url,
-        "hemi_dict": hemi_dict
+        "hemi_dict": hemispheres_dict
     }
 
 
